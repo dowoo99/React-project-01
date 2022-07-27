312c07c21e06b8af201637dc55f667108d184303
dowoo99 <dkalstn12@naver.com>
2022-07-27 14:08:55 +0900

    최종제출 css수정 완성본 form.css todo.css

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,12 +1,12 @@
 //전체레이아웃
-import TodoList from "./pages/TodoList";
+import TodoList from "./pages/TodoList.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <TodoList />
-    </div>
-  );
+    return (
+        <div className="App">
+            <TodoList />
+        </div>
+    );
 }
 
 export default App;
--- to-do/src/components/layout/Layout.jsx
+++ to-do/src/components/layout/Layout.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import "./style.css";
 
 const Layout = (props) => {
-  return <div className="layout">{props.children}</div>;
+    return <div className="layout">{props.children}</div>;
 };
 export default Layout;
